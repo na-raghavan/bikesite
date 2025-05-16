b23824fd300ea4faa9fe76d5451aa437f9945e02
na-raghavan <narasimhanaraghavan@gmail.com>
2025-05-16 12:57:30 -0700

    step 2

--- map.js
+++ map.js
@@ -12,3 +12,39 @@ const map = new mapboxgl.Map({
   minZoom: 5, // Minimum allowed zoom
   maxZoom: 18, // Maximum allowed zoom
 });
+
+map.on('load', async () => {
+  // --- Boston bike lanes ---
+  map.addSource('boston_route', {
+    type: 'geojson',
+    data:
+      'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+  });
+  map.addLayer({
+    id: 'bike-lanes-boston',
+    type: 'line',
+    source: 'boston_route',
+    paint: {
+      'line-color': '#32D400',  // bright green
+      'line-width': 3,
+      'line-opacity': 0.4,
+    },
+  });
+
+  // --- Cambridge bike lanes ---
+  map.addSource('cambridge_route', {
+    type: 'geojson',
+    data:
+      'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
+  });
+  map.addLayer({
+    id: 'bike-lanes-cambridge',
+    type: 'line',
+    source: 'cambridge_route',
+    paint: {
+      'line-color': '#FF8C00',  // bright orange
+      'line-width': 3,
+      'line-opacity': 0.4,
+    },
+  });
+});
